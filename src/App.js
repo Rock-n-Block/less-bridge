2023fc5a826d3c049ee80ee0a2edaed950980506
gavdniwe <dimanikus0106@gmail.com>
2021-05-30 17:47:26 +0300

    added content for chains block

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ function App() {
           {/*</div>*/}
 
           <Form />
-          {/*<IntegratedChains/>*/}
+          <IntegratedChains/>
         </div>
 
         <Footer />
--- src/components/IntegratedChains/index.js
+++ src/components/IntegratedChains/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import EthereumChain from '../../assets/icons/chains/ethereum-chain.svg'
+import BinanceChain from '../../assets/icons/chains/binance-chain.svg'
+import TronChain from '../../assets/icons/chains/tron-chain.svg'
+import PolygonChain from '../../assets/icons/chains/polygon-chain.svg'
 
 import './style.scss';
 
-const IntegratedChains=()=>{
-    return(
+const IntegratedChains = () => {
+    return (
         <div className="integrated-chains">
             <h2 className="integrated-chains__title">Chains Integrated</h2>
             <div className="integrated-chains__items">
                 <div className="integrated-chains__item">
-
+                    <img src={EthereumChain} alt="ethereum chain"/>
+                </div>
+                <div className="integrated-chains__item">
+                    <img src={BinanceChain} alt="ethereum chain"/>
+                </div>
+                <div className="integrated-chains__item">
+                    <img src={TronChain} alt="ethereum chain"/>
+                </div>
+                <div className="integrated-chains__item">
+                    <img src={PolygonChain} alt="ethereum chain"/>
                 </div>
             </div>
         </div>
